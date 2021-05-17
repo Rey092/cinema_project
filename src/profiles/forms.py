350a3a0caccbb00c3b3b573953934ba44183813f
rey092 <roman.cheburan@gmail.com>
2021-05-17 21:46:42 +0300

    UserProfile, profile form.

---
+++ src/profiles/forms.py
@@ -0,0 +1,84 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.forms import CharField, DateInput, EmailInput, ModelForm, PasswordInput, Select, TextInput
+
+from .models import UserProfile
+
+
+class UserProfileForm(LoginRequiredMixin, ModelForm):
+    new_password = CharField(
+        widget=PasswordInput(
+            attrs={'class': 'form-control', 'placeholder': 'Введите новый пароль'}),
+        label='Новый пароль', required=False)
+    confirm_password = CharField(
+        widget=PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Повторите пароль', }),
+        label='Повторите пароль', required=False)
+
+    class Meta:
+        model = UserProfile
+        fields = ['first_name', 'language', 'last_name', 'gender', 'username', 'phone_number',
+                  'email', 'birthday', 'address', 'city', 'cc_number']
+        widgets = {
+            'first_name': TextInput(attrs={
+                'required': 'required',
+                'class': 'form-control',
+                'placeholder': 'Input text',
+            }),
+            'language': Select(attrs={
+                'required': 'required',
+                'class': 'form-control',
+                'placeholder': 'Выберите язык',
+                'label': 'Язык'
+            }),
+            'last_name': TextInput(attrs={
+                'required': 'required',
+                'class': 'form-control',
+                'placeholder': 'Input text',
+            }),
+            'gender': Select(attrs={
+                'required': 'required',
+                'class': 'form-control',
+            }),
+            'username': TextInput(attrs={
+                'required': 'required',
+                'class': 'form-control',
+                'placeholder': 'Input username',
+            }),
+            'phone_number': TextInput(attrs={
+                'required': 'required',
+                'class': 'form-control',
+                'placeholder': 'Введите номер телефона',
+            }),
+            'email': EmailInput(attrs={
+                'required': 'required',
+                'class': 'form-control',
+                'placeholder': 'Short description',
+            }),
+            'birthday': DateInput(attrs={
+                'required': 'required',
+                'class': 'form-control',
+                'placeholder': 'Input text',
+            }),
+            'address': TextInput(attrs={
+                'required': 'required',
+                'class': 'form-control',
+                'placeholder': 'Input text',
+            }),
+            'city': Select(attrs={
+                'required': 'required',
+                'class': 'form-control',
+            }),
+            'cc_number': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Input text',
+            }),
+        }
+
+    field_order = ['first_name', 'language', 'last_name', 'gender', 'username', 'phone_number',
+                   'email', 'birthday', 'address', 'city', 'new_password', 'confirm_password', 'cc_number']
+
+    def clean(self):
+        cd = self.cleaned_data
+        if cd.get('new_password') != cd.get('confirm_password'):
+            self.add_error('confirm_password', 'passwords do not match !')
+            self.add_error('new_password', 'passwords do not match !')
+        return cd
